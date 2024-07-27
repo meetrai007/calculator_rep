a55d55867c972636cc4f56d22aa949eab3e94e2d
meetrai007 <harmeetsingh77216@gmail.com>
2024-07-27 13:02:35 +0530

    remove unwanted code

--- calculater_v2.py
+++ calculater_v2.py
@@ -1,37 +1,37 @@
 def calculate(num1, operator, num2):
-  """functon to take two inputs with one operator and return result after calculation"""
+    """functon to take two inputs with one operator and return result after calculation"""
 
-  if operator == "+":
-    return num1 + num2
-  elif operator == "-":
-    return num1 - num2
-  elif operator == "*":
-    return num1 * num2
-  elif operator == "/":
-    if num2 == 0:
-      print("Division by zero is not allowed")
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        if num2 == 0:
+            print("Division by zero is not allowed")
+        else:
+            return num1 / num2
     else:
-      return num1 / num2
-  else:
-    print("Invalid operator")
+        print("Invalid operator")
+
 
 while True:
-  try:
-    num1 = float(input("Enter the first number: "))
-    operator = input("Enter an operator (+, -, *, or /): ")
-    num2 = float(input("Enter the second number: "))
+    try:
+        num1 = float(input("Enter the first number: "))
+        operator = input("Enter an operator (+, -, *, or /): ")
+        num2 = float(input("Enter the second number: "))
 
-    result = calculate(num1, operator, num2)
-    print(f"{num1} {operator} {num2} = {result}")
+        result = calculate(num1, operator, num2)
+        print(f"{num1} {operator} {num2} = {result}")
 
-  except ValueError as e:
-    print("Invalid input. Please use numbers and valid operators.")
-  except ZeroDivisionError as e:
-    print("Error:", e)
+    except ValueError as e:
+        print("Invalid input. Please use numbers and valid operators.")
+    except ZeroDivisionError as e:
+        print("Error:", e)
 
-  print("Do you want to perform another calculation? (y/n)")
-  choice = input().lower()
-  if choice != "y":
-    break
-print("hello")
-print("hello sir")
+    print("Do you want to perform another calculation? (y/n)")
+    choice = input().lower()
+    if choice != "y":
+        break
+
