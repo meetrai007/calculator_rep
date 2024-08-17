1e29964d3345f9d8b77d7a0850844708cfd29363
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-17 22:00:41 +0530

    completed a version of calculator

--- calculater_v3.py
+++ calculater_v3.py
@@ -41,7 +41,7 @@ def squareroot():
     a=int(input("enter number:"))
     result=math.sqrt(a)
     return result
-def sign():
+def sin():
     a=int(input("enter number:"))
     result=math.sin(a)
     return result
@@ -53,5 +53,57 @@ def tan():
     a=int(input("enter number:"))
     result=math.tan(a)
     return result
+def log():
+    a=int(input("enter number:"))
+    result=math.log(a)
+    return result
 
-print(tan())
+
+while True:
+    logging.info("""
+    ###  welcome to the calculator ###
+                
+        choose a operation to make calculation 
+                1. addition
+                2. subtraction
+                3. multiply
+                4. devide
+                5. modulus
+                6. squre
+                7. power of number
+                8. squareroot
+                9. sin
+                10. cos
+                11. tan
+                12. log
+                13. exit the application
+    """)
+    choice=int(input("Enter your choice 1-12:"))
+
+    if choice==1:
+        print(addition())
+    if choice==2:
+        print(subtraction())
+    if choice==3:
+        print(multiply())
+    if choice==4:
+        print(devide())
+    if choice==5:
+        print(modulus())
+    if choice==6:
+        print(square())
+    if choice==7:
+        print(powerof_number())
+    if choice==8:
+        print(squareroot())
+    if choice==9:
+        print(sin())
+    if choice==10:
+        print(cos())
+    if choice==11:
+        print(tan())
+    if choice==12:
+        print(log())
+    if choice==13:
+        print("exiting the application")
+        break
