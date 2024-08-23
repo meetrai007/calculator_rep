5773a21636adc3217d3944da77f33e52e3e17287
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-23 21:44:48 +0530

    complete calculator gui version

--- gui_calculator.py
+++ gui_calculator.py
@@ -2,55 +2,103 @@ from tkinter import *
 
 def click(event):
     global scvalue
-    text=event.wedget.cget("text")
-    print(text)
-
-    if text=="=":
-        pass
-    if text=="c":
-        pass
+    text = event.widget.cget("text")
+    if text == "=":
+        try:
+            result = str(eval(scvalue.get()))
+            scvalue.set(result)
+        except Exception as e:
+            scvalue.set("Error")
+    elif text == "c":
+        scvalue.set("")
     else:
-        scvalue.set(scvalue.get()+text)
+        scvalue.set(scvalue.get() + text)
         screen.update()
 
-
-root= Tk()
-root.title("calculator")
-root.geometry("300x350")
+root = Tk()
+root.title("Calculator")
+root.geometry("350x400")
 root.configure(bg="black")
 
-scvalue=StringVar()
+scvalue = StringVar()
 scvalue.set("")
-screen=Entry(root,textvar=scvalue,font="lucida 40 bold")
-screen.pack(fill=X,ipadx=10,ipady=10)
-var=IntVar
-label=Label(root,text="",width=38,height=3,border=5).place(x=10,y=3)
+screen = Entry(root, textvar=scvalue, font="lucida 20 bold")
+screen.grid(row=0, column=0, columnspan=5, ipadx=8, ipady=8, padx=10, pady=10)
 
+b = Button(root, text="9", width=8, height=2)
+b.grid(row=1, column=1, padx=10, pady=10)
+b.bind("<Button-1>", click)
 
-button1=Button(root,text="c",width=8,height=2).place(x=10,y=70)
-button2=Button(root,text="%",width=8,height=2).place(x=80,y=70)
-button3=Button(root,text="/",width=8,height=2).place(x=150,y=70)
-button4=Button(root,text="*",width=8,height=2).place(x=220,y=70)
+b = Button(root, text="8", width=8, height=2)
+b.grid(row=1, column=2, padx=10, pady=10)
+b.bind("<Button-1>", click)
 
-button5=Button(root,text="9",width=8,height=2).place(x=10,y=120)
-button6=Button(root,text="8",width=8,height=2).place(x=80,y=120)
-button7=Button(root,text="7",width=8,height=2).place(x=150,y=120)
-button8=Button(root,text="-",width=8,height=2).place(x=220,y=120)
+b = Button(root, text="7", width=8, height=2)
+b.grid(row=1, column=3, padx=10, pady=10)
+b.bind("<Button-1>", click)
 
-button9=Button(root,text="6",width=8,height=2).place(x=10,y=170)
-button10=Button(root,text="5",width=8,height=2).place(x=80,y=170)
-button11=Button(root,text="4",width=8,height=2).place(x=150,y=170)
-button12=Button(root,text="+",width=8,height=2).place(x=220,y=170)
+b = Button(root, text="/", width=8, height=2)
+b.grid(row=1, column=4, padx=10, pady=10)
+b.bind("<Button-1>", click)
 
-button13=Button(root,text="3",width=8,height=2).place(x=10,y=220)
-button14=Button(root,text="2",width=8,height=2).place(x=80,y=220)
-button15=Button(root,text="1",width=8,height=2).place(x=150,y=220)
-button16=Button(root,text="X",width=8,height=2).place(x=220,y=220)
+b = Button(root, text="6", width=8, height=2)
+b.grid(row=2, column=1, padx=10, pady=10)
+b.bind("<Button-1>", click)
 
-button17=Button(root,text="0",width=8,height=2).place(x=10,y=270)
-button18=Button(root,text=".",width=8,height=2).place(x=80,y=270)
-button19=Button(root,text="00",width=8,height=2).place(x=150,y=270)
-button20=Button(root,text="=",width=8,height=2).place(x=220,y=270)
+b = Button(root, text="5", width=8, height=2)
+b.grid(row=2, column=2, padx=10, pady=10)
+b.bind("<Button-1>", click)
 
+b = Button(root, text="4", width=8, height=2)
+b.grid(row=2, column=3, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="*", width=8, height=2)
+b.grid(row=2, column=4, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="3", width=8, height=2)
+b.grid(row=3, column=1, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="2", width=8, height=2)
+b.grid(row=3, column=2, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="1", width=8, height=2)
+b.grid(row=3, column=3, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="+", width=8, height=2)
+b.grid(row=3, column=4, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text=".", width=8, height=2)
+b.grid(row=4, column=1, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="0", width=8, height=2)
+b.grid(row=4, column=2, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="%", width=8, height=2)
+b.grid(row=4, column=3, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="-", width=8, height=2)
+b.grid(row=4, column=4, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="00", width=8, height=2)
+b.grid(row=5, column=1, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="c", width=8, height=2)
+b.grid(row=5, column=2, padx=10, pady=10)
+b.bind("<Button-1>", click)
+
+b = Button(root, text="=", width=20, height=2)
+b.grid(row=5, column=3,columnspan=2, padx=10, pady=10)
+b.bind("<Button-1>", click)
 
 root.mainloop()
