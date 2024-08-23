9627a0f070a7b50d604c154e07b2fccad1102050
meetrai007 <harmeetsingh77216@gmail.com>
2024-08-23 21:15:09 +0530

    add some cose in gui calculator

--- gui_calculator.py
+++ gui_calculator.py
@@ -1,38 +1,56 @@
-import tkinter as tk
-from tkinter import messagebox
+from tkinter import *
 
-def addition():
-    a=int(entery.get())
-    b=int(entery2.get())
-    c=a+b
-    print(c)
-def sub():
-    a=int(entery.get())
-    b=int(entery2.get())
-    c=a-b
-    print(c)
-def multiply():
-    a=int(entery.get())
-    b=int(entery2.get())
-    c=a*b
-    print(c)
+def click(event):
+    global scvalue
+    text=event.wedget.cget("text")
+    print(text)
+
+    if text=="=":
+        pass
+    if text=="c":
+        pass
+    else:
+        scvalue.set(scvalue.get()+text)
+        screen.update()
 
 
-root= tk.Tk()
+root= Tk()
 root.title("calculator")
-root.geometry("300x400")
+root.geometry("300x350")
+root.configure(bg="black")
 
-var=tk.IntVar
-entery=tk.Entry(root)
-entery.pack()
-entery2=tk.Entry(root)
-entery2.pack()
+scvalue=StringVar()
+scvalue.set("")
+screen=Entry(root,textvar=scvalue,font="lucida 40 bold")
+screen.pack(fill=X,ipadx=10,ipady=10)
+var=IntVar
+label=Label(root,text="",width=38,height=3,border=5).place(x=10,y=3)
 
-button=tk.Button(root,text="add",command=addition)
-button.pack()
-button2=tk.Button(root,text="sub",command=sub)
-button2.pack()
-button3=tk.Button(root,text="mul",command=multiply)
-button3.pack()
+
+button1=Button(root,text="c",width=8,height=2).place(x=10,y=70)
+button2=Button(root,text="%",width=8,height=2).place(x=80,y=70)
+button3=Button(root,text="/",width=8,height=2).place(x=150,y=70)
+button4=Button(root,text="*",width=8,height=2).place(x=220,y=70)
+
+button5=Button(root,text="9",width=8,height=2).place(x=10,y=120)
+button6=Button(root,text="8",width=8,height=2).place(x=80,y=120)
+button7=Button(root,text="7",width=8,height=2).place(x=150,y=120)
+button8=Button(root,text="-",width=8,height=2).place(x=220,y=120)
+
+button9=Button(root,text="6",width=8,height=2).place(x=10,y=170)
+button10=Button(root,text="5",width=8,height=2).place(x=80,y=170)
+button11=Button(root,text="4",width=8,height=2).place(x=150,y=170)
+button12=Button(root,text="+",width=8,height=2).place(x=220,y=170)
+
+button13=Button(root,text="3",width=8,height=2).place(x=10,y=220)
+button14=Button(root,text="2",width=8,height=2).place(x=80,y=220)
+button15=Button(root,text="1",width=8,height=2).place(x=150,y=220)
+button16=Button(root,text="X",width=8,height=2).place(x=220,y=220)
+
+button17=Button(root,text="0",width=8,height=2).place(x=10,y=270)
+button18=Button(root,text=".",width=8,height=2).place(x=80,y=270)
+button19=Button(root,text="00",width=8,height=2).place(x=150,y=270)
+button20=Button(root,text="=",width=8,height=2).place(x=220,y=270)
+
 
 root.mainloop()
